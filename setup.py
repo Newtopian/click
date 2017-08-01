e94947eda38b3557c739697e7b90d4a4dcf70766
Eric Daigneault <eric.daigneault2@cn.ca>
2017-08-01 13:09:24 -0400

    changed version to 6.6.1 and deployed

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@ setup(
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
     ],
 )
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,8 @@ import pytest
 @pytest.fixture(scope='function')
 def runner(request):
     return CliRunner()
+
+
+if __name__ == '__main__':
+    import pytest
+    pytest.main()
